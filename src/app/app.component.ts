13cc6f82dc19c08bae32717a175ea1eef2b3ed56
Nemo <hackersaheadddd@gmail.com>
2020-06-26 15:03:23 +0300

    Создание frontend на Angular и вызов RESTful сервиса

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import {Task} from './entity/task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DemoAngRestful';
+  task: Task;
+  // вызов RESTful сервиса и запись в переменную
+  constructor(private http: HttpClient) {
+    this.http.get<Task>('http://localhost:8080/task').subscribe(result => {
+      this.task = result;
+      console.log(this.task);
+    });
+  }
 }
